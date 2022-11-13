2f5951b723214cbbd3503b0f947ac2baa00ebaea
Eric Fei <guanwha@gmail.com>
2022-11-13 17:58:50 +0800

    Added award -- add animation

--- src/components/Award.vue
+++ src/components/Award.vue
@@ -1,22 +1,22 @@
 <template>
   <section id="award" class="mx-auto w-[1104px] py-40 flex-ccc gap-[60px]">
     <!-- title -->
-    <div id="award-title" class="flex-rcc">
+    <div class="flex-rcc">
       <h2 class="text-cn1 text-h2 font-bold text-shadow-white px-9">
-        <span>區區修煉已經無法滿足了嗎？</span>
-        <span>還有比賽等著你！</span>
+        <span id="title-1">區區修煉已經無法滿足了嗎？</span>
+        <span id="title-2">還有比賽等著你！</span>
       </h2>
     </div>
     <!-- animation -->
     <div class="w-2/3 flex-ccc gap-2.5">
       <div class="w-full flex-rsbb">
-        <img src="@/assets/award/img_flag.svg" alt="flag">
-        <img src="@/assets/award/img_car.svg" alt="car">
+        <img id="flag" src="@/assets/award/img_flag.svg" alt="flag">
+        <img id="car" src="@/assets/award/img_car.svg" alt="car">
       </div>
-      <div class="line text-shadow-white"></div>
+      <div id="line" class="line text-shadow-white"></div>
     </div>
     <!-- review + award -->
-    <div class="flex-rcc gap-[60px]">
+    <div id="info" class="flex-rcc gap-[60px]">
       <!-- review -->
       <div :class="classCard">
         <div :class="classCardTop">
@@ -77,11 +77,40 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
+
 const classCard = `bg-cn4 rounded-card p-10 flex-ctc`;
 const classCardTop = `pb-4 flex-ccc`;
 const classCardTitle = `text-h2 text-cp1 font-bold`;
 const classCardBottom = `p-10 flex-ctl text-cn1`;
 const classCardInfoH5 = `text-h5 flex-rlc whitespace-nowrap gap-2`;
+
+onMounted(() => {
+  let title_1 = document.querySelector("#title-1");
+  let title_2 = document.querySelector("#title-2");
+  let info = document.querySelector("#info");
+  let flag = document.querySelector("#flag");
+  let car = document.querySelector("#car");
+  let line = document.querySelector("#line");
+
+  let tl = gsap.timeline().from(title_1, { y: 50, opacity: 0, duration: 0.5 })
+                          .from(info, { y: 50, opacity: 0, duration: 0.5 }, 0)
+                          .from(title_2, { opacity: 0, duration: 0.5 }, ">+=1")
+                          .to(car, { x: -line.clientWidth * 0.83, duration: 0.5 })
+                          .to(flag, { x: -999, y: -300, rotationZ: 360, duration: 0.5 }, ">-=25%");
+
+  // set trigger
+  ScrollTrigger.create({
+    trigger: '#award',
+    start: "50% center",
+    // scrub: 1,                                  // control by scroll
+    toggleActions: "play none none reverse",   // auto-animation
+    animation: tl,
+  });
+});
 </script>
 
 <style lang="scss" scoped>
