f26ffcc8d768268906e5af162e90aeab1605b1ff
Eric Fei <guanwha@gmail.com>
2022-11-14 06:50:56 +0800

    [Deploy] deploy to github page  -- test

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === "production"
-      ? "/week_1_parallax/"
-      : "./",
+  // base:
+  //   process.env.NODE_ENV === "production"
+  //     ? "/week_1_parallax/"
+  //     : "./",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
